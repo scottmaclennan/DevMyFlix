0acccfc1bc8fa78e9727d5f86d122a3bcaa15eda
scottmaclennan <scottmaclennan30@gmail.com>
2024-01-30 00:07:29 +0000

    added s3 bucket instance

--- src/pages/GenreExplore.tsx
+++ src/pages/GenreExplore.tsx
@@ -32,14 +32,6 @@ export function Component() {
     | CustomGenre
     | Genre
     | undefined;
-  // const { genreId } = useParams();
-  // const { data: genres } = useGetGenresQuery(MEDIA_TYPE.Movie);
-  // let genre: Genre | CustomGenre | undefined;
-  // if (isNaN(parseInt(genreId!))) {
-  //   genre = COMMON_TITLES.find((t) => t.apiString === genreId);
-  // } else {
-  //   genre = genres?.find((t) => t.id.toString() === genreId);
-  // }
   if (genre) {
     return <GridPage mediaType={MEDIA_TYPE.Movie} genre={genre} />;
   }
